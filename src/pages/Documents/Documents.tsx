85554d0799565c79b8ad94dfc3215eedfa51cbbb
Кирилл <mrkillraider@yandex.ru>
2024-02-19 13:40:46 +0300

    added home page

--- src/pages/Documents/Documents.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Documents() {
-    return (
-        <>Документы</>
-    )
-}
-
-export default Documents;
--- src/pages/Support/Support.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Support() {
-    return (
-        <>Документы</>
-    )
-}
-
-export default Support;
